30714371b8aeadd79ea81e9dac295968f34a359c
haveeru <suood_mohamed@hotmail.com>
2020-11-08 15:40:13 +0500

    topic request validation

---
+++ app/Http/Requests/TopicCreateRequest.php
@@ -0,0 +1,23 @@
+<?php
+//php artisan make:request TopicCreateRequest
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TopicCreateRequest extends FormRequest
+{
+
+    public function authorize()
+    {
+        return true;
+    }
+
+
+    public function rules()
+    {
+        return [
+            'title' => 'required|max:255',
+            'body' => 'required|max:200'
+        ];
+    }
+}
